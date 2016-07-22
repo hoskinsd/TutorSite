30d6bd5a35d3c86ddcd72238e2ffd854a121a7ef
dhoskin <adoug.hoskins@gmail.com>
2016-07-21 21:21:04 -0500

    Adding grading code and all other files

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
- 
 
-newWorksheet();
+
+//newWorksheet('addition');
 
 
 var loc_data;
 
 	// Do an ajax call for data and create a new worksheet.
-	function newWorksheet() {
+	function newWorksheet(type) {
 	var worksheetAPI = "http://174.49.32.201/TutorSite/cg/newWorkSheet.pl";
 	var string='';
 	$.getJSON( worksheetAPI, {
-		tags: "mount rainier",
-		tagmode: "any",
-		format: "json"
+		mode: "random",
+		type: "rando"
 	})
 		.done(function( data ) {
 			//console.log( 'hellos2' );
 			console.log( data[0].numerator );
 			loc_data = data;
-
-			var iterator=0;
-			for (let problem of data) {
-				paraId= iterator;
-				string += "<div>" + problem.numerator + ' + ' + problem.denominator + 
-						" = <input id=h" +iterator+" onclick='"+ 'addition(event,"'+paraId+'")' + "'><br><p id=r"+iterator+">&nbsp;</p></div>";
-				iterator=iterator+1;
-			}
+			string = create_html(data,type);
 			$('#prob1').html(string);
 		})
 
 	};
 
+
+	function create_html (data,type){
+			var string='';
+			var iterator=0;
+			var iterator;
+			for (let problem of data) {
+//				iterator= iterator;
+				if (type == 'addition'){
+					string += "<div>" + problem.numerator + ' + ' + problem.denominator + 
+						" = <input id=h" +iterator+" onclick='"+ 'addition(event,"'+iterator+'")' + "'><br><p id=r"+iterator+">&nbsp;</p></div>";
+				} else {
+					string += "<div>" + problem.numerator + ' - ' + problem.denominator + 
+						" = <input id=h" +iterator+" onclick='"+ 'subtraction(event,"'+iterator+'")' + "'><br><p id=r"+iterator+">&nbsp;</p></div>";
+				}
+				iterator=iterator+1;
+			}
+			return string;
+	}
 
 	function addition (event,id){
 	var ret_val;
@@ -39,8 +49,29 @@ var loc_data;
 
 		$("#h"+id).keyup(function(event){
 		console.log("event");
-			 if(event.keyCode == 13){
+			 if((event.keyCode == 13) || (event.keyCode == 9)){
 					if ($('#h'+id).val() == loc_data[id].numerator + loc_data[id].denominator){
+						$("#r"+id).html("Whoo Hooo");
+					} else {
+						$("#r"+id).html("Sorry");
+					}
+					loc_data[id].result = 'Correct';
+		console.log("Result: "+loc_data[id].result);
+			 }
+		});
+
+
+	}
+
+	function subtraction (event,id){
+	var ret_val;
+	var loc_index;
+//	console.log ("add: "+id);
+
+		$("#h"+id).keyup(function(event){
+		console.log("event");
+			 if((event.keyCode == 13) || (event.keyCode == 9)){
+					if ($('#h'+id).val() == loc_data[id].numerator - loc_data[id].denominator){
 						$("#r"+id).html("Whoo Hooo");
 					} else {
 						$("#r"+id).html("Sorry");
@@ -48,7 +79,14 @@ var loc_data;
 
 			 }
 		});
+	}
 
+	function gradeWorksheet() {
+
+		// loop for number of problems
+		// add up score
+		// display score
+		// send score back to the server
 
 	}
 
--- cgi-bin/newWorkSheet.pl
+++ cgi-bin/newWorkSheet.pl
@@ -1,16 +1,27 @@
 #!/usr/bin/perl
-
+use CGI;
 use JSON;
 #use JSON::XS;
+use Data::Dumper;
 
 
+my $q    = new CGI;
+my $loc_mode = $q->param( "mode" ) || shift;
+open FILE,">param.txt";
+open DATA,"<data.txt";
+my $next = <DATA>;
+close DATA;
+print "Next: $next\n";
+
+print "Type: $loc_mode*\n";
+print FILE "Dumper: " .Dumper($q)."\n";
 my @rec_array;
 
-my %rec_hash1 = ('numerator' => 50, 'denominator' => 20, 'result' => 30);
-my %rec_hash2 = ('numerator' => 10, 'denominator' => 20, 'result' => 30);
-my %rec_hash3 = ('numerator' => 10, 'denominator' => 20, 'result' => 30);
-my %rec_hash4 = ('numerator' => 10, 'denominator' => 20, 'result' => 30);
-my %rec_hash5 = ('numerator' => 10, 'denominator' => 20, 'result' => 30);
+my %rec_hash1 = ('numerator' => int(rand(20)), 'denominator' => int(rand(20)), 'result' => int(rand(20)));
+my %rec_hash2 = ('numerator' => int(rand(20)), 'denominator' => int(rand(20)), 'result' => int(rand(20)));
+my %rec_hash3 = ('numerator' => int(rand(20)), 'denominator' => int(rand(20)), 'result' => int(rand(20)));
+my %rec_hash4 = ('numerator' => int(rand(20)), 'denominator' => int(rand(20)), 'result' => int(rand(20)));
+my %rec_hash5 = ('numerator' => int(rand(20)), 'denominator' => int(rand(20)), 'result' => int(rand(20)));
 
 push @rec_array, \%rec_hash1;
 push @rec_array, \%rec_hash2;
@@ -21,70 +32,4 @@ push @rec_array, \%rec_hash5;
 my $json = encode_json \@rec_array;
 print "Content-type: text/html\r\n\r\n";
 print "$json\n";
-
-=comment
-print "Content-type: text/html\r\n\r\n";
-#print "<!DOCTYPE html>"
-print <<EOF;
-
-
-<id1>
-	<numerator>
-	10
-	</numerator>
-	<denominator>
-	20
-	</denominator>
-	<result>
-	30
-	</result>
-</id1>
-<id2>
-	<numerator>
-	10
-	</numerator>
-	<denominator>
-	20
-	</denominator>
-	<result>
-	30
-	</result>
-</id2>
-<id3>
-	<numerator>
-	10
-	</numerator>
-	<denominator>
-	20
-	</denominator>
-	<result>
-	30
-	</result>
-</id3>
-<id4>
-	<numerator>
-	10
-	</numerator>
-	<denominator>
-	20
-	</denominator>
-	<result>
-	30
-	</result>
-</id4>
-<id5>
-	<numerator>
-	10
-	</numerator>
-	<denominator>
-	20
-	</denominator>
-	<result>
-	30
-	</result>
-</id5>
-
-EOF
-=cut
-
 
