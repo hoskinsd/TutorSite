66dae0a8143d0fd9da7f50f17dd475dae1509460
dhoskin <adoug.hoskins@gmail.com>
2016-08-07 21:04:06 -0500

    add login popup and animation

--- cgi-bin/TutorSite.pl
+++ cgi-bin/TutorSite.pl
@@ -9,19 +9,52 @@ print <<EOF;
 <head>
 	<script src="http://www.w3schools.com/lib/w3data.js"></script>
 	<link rel="import" href="html_head.html">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<link rel="import" href="http://174.49.32.201/TutorSite/html_head.html">
+	<script type = "text/javascript" src="jquery-3.0.0.js"></script>
+	<script type = "text/javascript"  src="http://174.49.32.201/TutorSite/app.js"></script>
 
 	<style>
 		body {
 		  background: url(http://174.49.32.201/TutorSite/school.jpeg) no-repeat;
 		  background-size: 100%;
 		}
+		
+		#login{
+		  top: 0;
+		  left: 80%;
+		  width: 10%;
+		  height: 10%;
+		  position: relative;
+		}
+
+		#header,
+		#content {
+		  width: 100%;
+		  height: 80%;
+		  position: absolute;
+		  top: 0;
+		  left: 0;
+		}
+		#inbutton{
+		  top: 65%;
+		  left: 10;
+		  height: 10%;
+		  width: 10%;
+		  position: absolute;
+		}
+		#login {
+		  z-index: 10;
+		}
 	</style>
 </head>
 <body ng-app="workSheet">
 
-	<div w3-include-html="http://174.49.32.201/TutorSite/header.html"></div>
-	<div w3-include-html="http://174.49.32.201/TutorSite/content.html"></div>
-
+		<div id="login"></div>
+		<div id='header' w3-include-html="http://174.49.32.201/TutorSite/header.html"></div>
+		<div id='content' w3-include-html="http://174.49.32.201/TutorSite/content.html"></div>
+	
+		<div id='inbutton'>	<input id="loginbutton" type="button" value="Login" onclick="login_popup();" /></div>
 </body>
 </html>
 
